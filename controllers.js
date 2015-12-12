cdfc002d05e735da63cdf6175c2daf17eab00ea3
Bervyn Co <bervyn_co2010@yahoo.com>
2015-12-12 18:02:49 +0800

    Add maps

--- controllers.js
+++ controllers.js
@@ -17,6 +17,39 @@ webApp.factory('ProductFactory',function($http){
 });
 
 //14.591158, 120.996992
+webApp.factory('MapFactory', function(){
+    var mapOptions = {
+                zoomControl: true,
+                zoomControlOptions: {
+                            style: google.maps.ZoomControlStyle.MEDIUM,
+                            position: google.maps.ControlPosition.LEFT_BOTTOM
+                },
+                panControl: true,
+                panControlOptions:{
+                            style: google.maps.ZoomControlStyle.MEDIUM,
+                            position: google.maps.ControlPosition.LEFT_CENTER
+                },
+                zoom: 18,
+                center: new google.maps.LatLng(14.591143, 120.997083),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    // Create the legend and display on the map
+    
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    return map;
+    
+});
+webApp.controller('MapController',
+  function($scope, MapFactory){
+    var map = MapFactory;
+    google.maps.event.addDomListener(window, "load", map);
+    map.setCenter(new google.maps.LatLng(14.591143, 120.997083));
+    var myLatLng = {lat: 14.591143, lng: 120.997083};
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map
+    });
+});
 webApp.controller('ProductController', ['$scope', 'ProductFactory',
   function($scope, ProductFactory){
 
